337bf9d952d4bcfa4dc9d04b303afd822eab1dd8
Sharath K Shetty <sharath@epicode.in>
2025-10-18 20:05:38 +0530

    minor changes

--- ira_bot_compose_json_schema.cpp
+++ ira_bot_compose_json_schema.cpp
@@ -3,7 +3,6 @@ const char* ConfFileSchema = R"(
     "$schema": "http://json-schema.org/draft-07/schema#",
     "type": "object",
     "properties": {
-      "use_self_signed_certificate": {"type": "boolean", "enum" : [true,false] },
       "wss_certificate_file": {"type": "string"},
       "wss_private_key_file": {"type": "string"},
       "wss_dh_params_file": {"type": "string"},
@@ -11,10 +10,10 @@ const char* ConfFileSchema = R"(
       "http_proxy_server": {"type": "string", "minLength": 2},
 	  "ws_port": {"type": "integer", "minimum": 2000 },
 	  "wss_port": {"type": "integer", "minimum": 2000 },
-      "request_subject": {"type": "string", "minLength": 5},
+      "request_subject": {"type": "string", "minLength": 5}
     },
 	"anyOf": [{"required": ["ws_port"]},{"required": ["wss_port"]}],
-    "required": ["use_self_signed_certificate"]
+    "required": ["wss_certificate_file","wss_private_key_file","wss_dh_params_file"]
 })";
 
 const char* IraRequestSchema = R"(
