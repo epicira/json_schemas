d491a7fd8e7b41bc6bb29076ef2cafa9f98d4613
Daryl Correa <darylalsc@gmail.com>
2026-08-18 10:14:40 +0530

    iraacd

--- ira_acd_json_schema.hpp
+++ ira_acd_json_schema.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <array>
 
 namespace IraACD
 {
@@ -11,11 +12,11 @@ inline constexpr const char *C_AddAgentSchema = R"({
             "type": "object",
             "properties": {
                 "tenant_id": {"type": "string"},
-                "user_id": {"type": "string"},
+                "id": {"type": "string"},
                 "extension": {"type": "string"}
             },
             "additionalProperties": false,
-            "required": ["tenant_id", "user_id", "extension"]
+            "required": ["tenant_id", "id", "extension"]
         }
     },
     "additionalProperties": false,
@@ -31,12 +32,12 @@ inline constexpr const char *C_UpdateAgentSchema = R"({
             "type": "object",
             "properties": {
                 "tenant_id": {"type": "string"},
-                "user_id": {"type": "string"},
+                "id": {"type": "string"},
                 "extension": {"type": "string"}
             },
             "additionalProperties": false,
             "minProperties": 3,
-            "required": ["tenant_id", "user_id"]
+            "required": ["tenant_id", "id"]
         }
     },
     "additionalProperties": false,
@@ -52,10 +53,10 @@ inline constexpr const char *C_DeleteAgentSchema = R"({
             "type": "object",
             "properties": {
                 "tenant_id": {"type": "string"},
-                "user_id": {"type": "string"},
+                "id": {"type": "string"},
             },
             "additionalProperties": false,
-            "required": ["tenant_id", "user_id"]
+            "required": ["tenant_id", "id"]
         }
     },
     "additionalProperties": false,
@@ -91,6 +92,7 @@ inline constexpr const char *C_AddQueueSchema = R"({
                 "close_message": {"type": "string"},
                 "whisper_tone": {"type": "string"}
             },
+            "additionalProperties": false,
             "required": [
                 "tenant_id", "name", "priority", "size", "strategy",
                 "active", "direction", "acw_timeout",
@@ -98,6 +100,167 @@ inline constexpr const char *C_AddQueueSchema = R"({
             ]
         }
     },
+    "additionalProperties": false,
+    "required": ["event_name", "event_data"]
+})";
+
+inline constexpr const char *C_UpdateQueueSchema = R"({
+    "$schema": "http://json-schema.org/draft-07/schema#",
+    "type": "object",
+    "properties": {
+        "event_name": {"type": "string"},
+        "event_data": {
+            "type": "object",
+            "properties": {
+                "tenant_id": {"type": "string"},
+                "id": {"type": "string"},
+                "name": {"type": "string"},
+                "priority": {"type": "integer"},
+                "size": {"type": "integer"},
+                "strategy": {"type": "string"},
+                "active": {"type": "boolean"},
+                "direction": {"type": "string"},
+                "acw_timeout": {"type": "integer"},
+                "max_calls": {"type": "integer"},
+                "abandonment_seconds": {"type": "integer"},
+                "open_time": {"type": "string"},
+                "hold_music": {"type": "string"},
+                "hold_messages": {
+                    "type": "array",
+                    "items": {"type": "string"}
+                },
+                "agent_hold_music": {"type": "string"},
+                "open_message": {"type": "string"},
+                "close_message": {"type": "string"},
+                "whisper_tone": {"type": "string"}
+            },
+            "additionalProperties": false,
+            "minProperties": 3,
+            "required": ["tenant_id", "id"]
+        }
+    },
+    "additionalProperties": false,
+    "required": ["event_name", "event_data"]
+})";
+
+inline constexpr const char *C_DeleteQueueSchema = R"({
+    "$schema": "http://json-schema.org/draft-07/schema#",
+    "type": "object",
+    "properties": {
+        "event_name": {"type": "string"},
+        "event_data": {
+            "type": "object",
+            "properties": {
+                "tenant_id": {"type": "string"},
+                "id": {"type": "string"},
+            },
+            "additionalProperties": false,
+            "required": ["tenant_id", "id"]
+        }
+    },
+    "additionalProperties": false,
+    "required": ["event_name", "event_data"]
+})";
+
+
+inline constexpr const char *C_AddAgentQueueMapSchema = R"({
+    "$schema": "http://json-schema.org/draft-07/schema#",
+    "type": "object",
+    "properties": {
+        "event_name": {"type": "string"},
+        "event_data": {
+            "type": "object",
+            "properties": {
+                "tenant_id": {"type": "string"},
+                "agent_id": {"type": "string"},
+                "queue_id": {"type": "string"}
+                "skill_level": {"type": "integer"}
+            },
+            "additionalProperties": false,
+            "required": ["tenant_id", "agent_id", "queue_id", "skill_level"]
+        }
+    },
+    "additionalProperties": false,
+    "required": ["event_name", "event_data"]
+})";
+
+inline constexpr const char *C_UpdateAgentQueueMapSchema = R"({
+    "$schema": "http://json-schema.org/draft-07/schema#",
+    "type": "object",
+    "properties": {
+        "event_name": {"type": "string"},
+        "event_data": {
+            "type": "object",
+            "properties": {
+                "tenant_id": {"type": "string"},
+                "agent_id": {"type": "string"},
+                "queue_id": {"type": "string"}
+                "skill_level": {"type": "integer"}
+            },
+            "additionalProperties": false,
+            "required": ["tenant_id", "agent_id", "queue_id", "skill_level"]
+        }
+    },
+    "additionalProperties": false,
+    "required": ["event_name", "event_data"]
+})";
+inline constexpr const char *C_DeleteAgentQueueMapSchema = R"({
+    "$schema": "http://json-schema.org/draft-07/schema#",
+    "type": "object",
+    "properties": {
+        "event_name": {"type": "string"},
+        "event_data": {
+            "type": "object",
+            "properties": {
+                "tenant_id": {"type": "string"},
+                "agent_id": {"type": "string"},
+                "queue_id": {"type": "string"}
+            },
+            "additionalProperties": false,
+            "required": ["tenant_id", "agent_id", "queue_id"]
+        }
+    },
+    "additionalProperties": false,
+    "required": ["event_name", "event_data"]
+})";
+
+inline constexpr const char *C_AddTaskSchema = R"({
+    "$schema": "http://json-schema.org/draft-07/schema#",
+    "type": "object",
+    "properties": {
+        "event_name": {"type": "string"},
+        "event_data": {
+            "type": "object",
+            "properties": {
+                "tenant_id": {"type": "string"},
+                "call_uuid": {"type": "string"},
+                "queue_id": {"type": "string"}
+            },
+            "additionalProperties": false,
+            "required": ["tenant_id", "call_uuid", "queue_id"]
+        }
+    },
+    "additionalProperties": false",
+    "required": ["event_name", "event_data"]
+})";
+
+inline constexpr const char *C_AgentStatusSchema = R"({
+    "$schema": "http://json-schema.org/draft-07/schema#",
+    "type": "object",
+    "properties": {
+        "event_name": {"type": "string"},
+        "event_data": {
+            "type": "object",
+            "properties": {
+                "tenant_id": {"type": "string"},
+                "agent_id": {"type": "string"},
+                "state": {"type": "integer"}
+            },
+            "additionalProperties": false"
+            "required": ["tenant_id", "agent_id", "state"]
+        }
+    },
+    "additionalProperties": false",
     "required": ["event_name", "event_data"]
 })";
 
